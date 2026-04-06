83b6948b15592025652ada659ac4c4af8b61bead
WenCheng-laptop <fofolongkk@gmail.com>
2026-04-06 14:41:10 +0800

    完成 C標準函式庫

--- common.c
+++ common.c
@@ -71,3 +71,36 @@ void printf(const char *fmt, ...) {
 end:
     va_end(vargs);
 }
+
+void *memcpy(void *dst, const void *src, size_t n) {
+    uint8_t *d = (uint8_t *) dst;
+    const uint8_t *s = (const uint8_t *) src;
+    while (n--)
+        *d++ = *s++;
+    return dst;
+}
+
+void *memset(void *buf, char c, size_t n) {
+    uint8_t *p = (uint8_t *) buf;
+    while (n--)
+        *p++ = c;
+    return buf;
+}
+
+char *strcpy(char *dst, const char *src) {
+    char *d = dst;
+    while (*src)           // 只要還沒讀到字串結尾的 '\0' (數值為 0)，就繼續跑
+        *d++ = *src++;     // 邊複製、邊把指標往後推 (上一階段學過的極致寫法)
+    *d = '\0';             // 煞車！手動在目的地補上結尾符號
+    return dst;
+}
+
+int strcmp(const char *s1, const char *s2) {
+    while (*s1 && *s2) {       // 只要兩個字串都還沒走到結尾 '\0'
+        if (*s1 != *s2)        // 只要發現有一個字元不一樣
+            break;             // 立刻中斷迴圈，準備判刑！
+        s1++;                  // 一樣的話，就繼續檢查下一個字元
+        s2++;
+    }
+    return *(unsigned char *)s1 - *(unsigned char *)s2; // 判決出爐
+}
